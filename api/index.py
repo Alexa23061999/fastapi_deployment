6401d4a66d9f4b2f7715cad766ac3a688f826378
PYTHON-ALEX <PYTHON-ALEX@GK.PHYTHON>
2024-09-26 13:30:47 +0530

    Code is complted

---
+++ api/index.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import numpy as np
+import joblib
+from pydantic import BaseModel
+from sklearn.datasets import load_iris
+
+
+target_names = load_iris().target_names
+app = FastAPI()
+
+
+class Input(BaseModel):
+    features: list
+
+
+md = joblib.load('./api/mlmodel.joblib')
+
+@app.post('/predict')
+
+def predict(data: Input):
+
+    x_input = np.array(data.features).reshape(1, -1)
+    prediction = md.predict(x_input)
+    return {"prediction": target_names[prediction[0]]}
