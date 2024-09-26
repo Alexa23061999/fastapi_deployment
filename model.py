c8ed377b62e581d924e5064d073434331c810086
Alex <alexa23061999@gmail.com>
2024-09-26 16:25:57 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,39 @@
-import joblib
+import pickle
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
 iris = load_iris()
-
 x = iris.data
 y = iris.target
 
-
 rf = RandomForestClassifier().fit(x, y)
 
-joblib.dump(rf, './api/mlmodel.joblib')
+with open('./api/mlmodel.pkl', 'wb') as f:
+    pickle.dump(rf, f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import joblib
+# from sklearn.datasets import load_iris
+# from sklearn.ensemble import RandomForestClassifier
+
+# iris = load_iris()
+
+# x = iris.data
+# y = iris.target
+
+
+# rf = RandomForestClassifier().fit(x, y)
+
+# joblib.dump(rf, './api/mlmodel.joblib')
